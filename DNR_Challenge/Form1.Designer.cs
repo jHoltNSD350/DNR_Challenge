24d7eb28e1035faf1ffe3f423b6d7e00ae563da2
Joseph Holtkamp <josephh1@iastate.edu>
2021-01-18 15:48:37 -0600

    initial commit

---
+++ DNR_Challenge/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace DNR_Challenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEntry = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(427, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Below enter DNA sequence to be uncorrupted or searched:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter DNA sequence: ";
+            // 
+            // txtEntry
+            // 
+            this.txtEntry.Location = new System.Drawing.Point(207, 49);
+            this.txtEntry.Name = "txtEntry";
+            this.txtEntry.Size = new System.Drawing.Size(414, 26);
+            this.txtEntry.TabIndex = 2;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(218, 149);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(140, 39);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(207, 93);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 26);
+            this.txtSearch.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sequence to search for: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 200);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtEntry);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "DNA Uncorrupter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEntry;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ DNR_Challenge/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace DNR_Challenge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Once clicked, the submit button takes the entered text and filters the DNA string
+        /// 
+        /// Once filtered, the string is formatted.
+        /// The formatted string is displayed as well as the count of the specified pattern by the user.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //Retrieve the entered DNA sequence and search sequence
+            string corruptedText = txtEntry.Text;
+            string searchSequence = txtSearch.Text.Trim().ToUpper();
+
+            bool searching = searchSequence.Length > 0;
+
+            //filter the search sequence
+            if(searching && UncorruptSequence(searchSequence).Trim().Length != 4)
+            {
+                MessageBox.Show("This program is only designed to search for sets of 4\r\n" +
+                    "valid nucleotide symbols at a time.\r\n\r\nTry again.");
+                return;
+            }
+
+            //Filter and format the DNA sequence
+            string cleanDNA = UncorruptSequence(corruptedText);
+
+            StringBuilder sb = new StringBuilder();
+
+            //First, display the filtered and formatted DNA sequence
+            sb.AppendLine("The clean DNA sequence is: ");
+            sb.AppendLine(Regex.Replace(cleanDNA, ".{30}", "$0\r\n"));
+            sb.AppendLine();
+
+            if (searching)
+            {
+                sb.Append("The occurances of sequence '" + searchSequence + "': ");
+
+                /*
+                 * The uncorrupted DNA sequence is here searched for a given sequence
+                 * 
+                 * ASSUMPTION: 
+                 * the assumption is that only in a pregrouped sequence is the pattern
+                 * to be counted. 
+                 * 
+                 * For example, 'ATCC TGCC' contains the given pattern of 'CCTG', but it is split
+                 * between two sequences of four and therefore not counted. 
+                 */
+                int count = Regex.Matches(cleanDNA, searchSequence).Count;
+                sb.AppendLine(count.ToString());
+            }
+
+            //The result is displayed in a message box
+            MessageBox.Show(sb.ToString());
+        }
+
+        /// <summary>
+        /// This method uncorrupts the DNA text sequence via regular expressions. 
+        /// </summary>
+        /// <param name="input">The possibly corrupted DNA text</param>
+        /// <returns>The uncorrupted and reformatted version of the original DNA sequence</returns>
+        private static string UncorruptSequence(string input)
+        {
+            //This statement removes all characters that are not A, C, G, or T(case insensitive).
+            string uncorrupted = Regex.Replace(input, "[^aAcCgGtT]", "").ToUpper();
+
+            //This statement then segments all of the text into sequences of 4 characters with a space following. 
+            string reformatted = Regex.Replace(uncorrupted, ".{4}", "$0 ");
+
+            return reformatted;
+        }
+    }
+}
